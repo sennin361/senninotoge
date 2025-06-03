43ddba278f98349fcae92132d2d02fea4bba282b
sennin361 <pokeopoke9@gmail.com>
2025-06-04 06:33:00 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// service-worker.js - キャッシュ制御 const CACHE_NAME = 'sennin-ongame-cache-v1'; const urlsToCache = [ '/', '/index.html', '/style.css', '/script.js', '/score.js', '/manifest.json', '/icon-192.png', '/icon-512.png' ];
+
+self.addEventListener('install', event => { event.waitUntil( caches.open(CACHE_NAME).then(cache => { return cache.addAll(urlsToCache); }) ); });
+
+self.addEventListener('fetch', event => { event.respondWith( caches.match(event.request).then(response => { return response || fetch(event.request); }) ); });
+
+self.addEventListener('activate', event => { event.waitUntil( caches.keys().then(cacheNames => { return Promise.all( cacheNames.map(cacheName => { if (cacheName !== CACHE_NAME) { return caches.delete(cacheName); } }) ); }) ); });
+
